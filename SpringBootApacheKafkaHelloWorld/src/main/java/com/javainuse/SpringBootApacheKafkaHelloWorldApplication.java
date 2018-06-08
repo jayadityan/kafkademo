34441fb1b7622f03abe6ceb9b18392e72860ef28
jayaditya <jayaditya.n@gmail.com>
2018-06-08 15:03:06 +1000

    Adding the Kafka Producer Project

---
+++ SpringBootApacheKafkaHelloWorld/src/main/java/com/javainuse/SpringBootApacheKafkaHelloWorldApplication.java
@@ -0,0 +1,14 @@
+package com.javainuse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootApacheKafkaHelloWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootApacheKafkaHelloWorldApplication.class, args);
+	}
+	
+	
+}
